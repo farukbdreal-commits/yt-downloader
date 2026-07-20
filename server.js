8b34686bedb8c9ef0d9121b3e700cf133b727249
farukbdreal-commits <farukbdreal@gmail.com>
2026-07-20 20:41:28 +0600

    Update server.js

--- server.js
+++ server.js
@@ -6,81 +6,48 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req,res)=>{
-    res.send(`
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <title>YT Downloader</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
+res.send(`
+<h2>YouTube Downloader</h2>
 
-    <body style="text-align:center;font-family:Arial;padding-top:50px">
+<form action="/download">
+<input name="url" placeholder="YouTube Link" style="width:80%;padding:10px">
+<br><br>
+<button>Download MP4</button>
+</form>
 
-    <h1>YouTube Video Downloader 🚀</h1>
-
-    <form action="/download">
-    <input 
-    name="url"
-    placeholder="Paste YouTube Link"
-    style="width:80%;padding:12px"
-    required>
-
-    <br><br>
-
-    <button style="padding:12px 25px">
-    Download MP4
-    </button>
-
-    </form>
-
-    </body>
-    </html>
-    `);
+`);
 });
 
 
 app.get("/download", async(req,res)=>{
 
-    const url=req.query.url;
+const url=req.query.url;
 
-    if(!url){
-        return res.send("Link দিন");
-    }
+if(!url){
+return res.send("Link দিন");
+}
 
+try{
 
-    try{
+res.header(
+"Content-Disposition",
+'attachment; filename="video.mp4"'
+);
 
-        if(!ytdl.validateURL(url)){
-            return res.send("Invalid YouTube URL");
-        }
-
-
-        res.setHeader(
-            "Content-Disposition",
-            'attachment; filename="video.mp4"'
-        );
+ytdl(url,{
+quality:"highestvideo"
+}).pipe(res);
 
 
-        ytdl(url,{
-            quality:"highestvideo"
-        }).pipe(res);
+}catch(e){
 
+res.send("Video download error");
 
-    }catch(error){
-
-        console.log(error);
-
-        res.status(500).send(
-        "ভিডিও আনতে সমস্যা হয়েছে"
-        );
-
-    }
+}
 
 });
 
 
 app.listen(PORT,()=>{
-console.log(
-`Server running on ${PORT}`
-);
+console.log("Server running");
 });
